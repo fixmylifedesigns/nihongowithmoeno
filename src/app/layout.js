82131c90a36c844e71bb8557171359c70c3d4b57
Irving Duran <ijd.irving@gmail.com>
2025-02-13 03:57:54 +0900

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,10 +9,50 @@ const geist = Geist({
 });
 
 export const metadata = {
-  title: "Japanese with Moeno - Learn Japanese the Authentic Way",
+  title: "Learn Japanese with Moeno | Private Lessons & Conversation Practice",
   description:
-    "Learn Japanese through immersive, one-on-one lessons with a native speaker. Private lessons for beginners and conversation practice for advanced learners.",
+    "Master Japanese with Moeno, a native speaker! Private lessons for beginners, JLPT prep, and conversation practice. Learn Japanese online the authentic way!",
+  keywords: [
+    "Learn Japanese",
+    "Japanese lessons",
+    "Japanese tutor",
+    "Japanese conversation practice",
+    "JLPT preparation",
+    "Online Japanese classes",
+    "Japanese teacher",
+    "Japanese with Moeno",
+    "Study Japanese online",
+    "Japanese language learning",
+    "Japan",
+    "Japanese Teacher"
+  ],
+  author: "Moeno | Nihongo with Moeno",
+  openGraph: {
+    title: "Learn Japanese with Moeno | Private Lessons & Conversation Practice",
+    description:
+      "Master Japanese with Moeno, a native speaker! Private lessons for beginners, JLPT prep, and conversation practice. Learn Japanese online the authentic way!",
+    url: "https://nihongowithmoeno.com",
+    type: "website",
+    site_name: "Nihongo with Moeno",
+    images: [
+      {
+        url: "https://nihongowithmoeno.com/images/nihongowithmoeno.png",
+        width: 1200,
+        height: 630,
+        alt: "Learn Japanese with Moeno",
+      },
+    ],
+  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Learn Japanese with Moeno | Private Lessons & Conversation Practice",
+  //   description:
+  //     "Master Japanese with Moeno, a native speaker! Private lessons for beginners, JLPT prep, and conversation practice. Learn Japanese online the authentic way!",
+  //   site: "@yourtwitterhandle",
+  //   image: "https://nihongowithmoeno.com/images/nihongowithmoeno.png",
+  // },
 };
+
 
 export default function RootLayout({ children }) {
   return (
